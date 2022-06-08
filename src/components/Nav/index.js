17ae234e239466f1ac2e1ab20dd6d7c415ced363
Kaia Scheirman <kfscheirman18@gmail.com>
2022-06-07 19:56:32 -0700

    add categories to the navigation bar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 
 function Nav() {
+    const categories = [
+        {
+            name: 'commercial',
+            description: 'Photos of grocery stores, food trucks, and other commercial projects'
+        },
+        { name: 'portraits', description: 'Portraits of people in my life' },
+        { name: 'food', description: 'Delicious delicacies' },
+        {
+            name: 'landscape',
+            description: 'Fields, farmhouses, waterfalls, and the beauty of nature'
+        }
+    ];
+
+    function categorySelected(name) {
+        console.log(`${name} clicked`);
+    };
+
     return (
         <header>
             <h2>
@@ -16,6 +33,14 @@ function Nav() {
                     <li>
                         <span>Contact</span>
                     </li>
+                    {/* when mapping over an array in JSX, return only a single JSX element */}
+                    {categories.map((category) => (
+                        // when mapping over anything in JSX, the outermost element must have a key attribute that is unique
+                        // this helps React keep track of items in the virtual DOM
+                        <li className='mx-1' key={category.name}>
+                            <span onClick={() => categorySelected(category.name)}>{category.name}</span>
+                        </li>
+                    ))}
                 </ul>
             </nav>
         </header>
