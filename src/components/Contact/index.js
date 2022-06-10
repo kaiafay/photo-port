d8615f8058709110708ddf49dff1d987d2dc4393
Kaia Scheirman <kfscheirman18@gmail.com>
2022-06-09 21:30:31 -0700

    conditionally render error message for form inputs

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -12,7 +12,7 @@ function ContactForm() {
     function handleChange(e) {
         if(e.target.name === 'email') {
             const isValid = validateEmail(e.target.value);
-            
+
             if(!isValid) {
                 setErrorMessage('Your email is invalid.');
             } else {
@@ -42,16 +42,21 @@ function ContactForm() {
             <form id='contact-form' onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor='name'>Name:</label>
-                    <input type='text' defaultValue={name} onChange={handleChange} name='name' />
+                    <input type='text' defaultValue={name} onBlur={handleChange} name='name' />
                 </div>
                 <div>
                     <label htmlFor='email'>Email address:</label>
-                    <input type='email' defaultValue={email} onChange={handleChange} name='email' />
+                    <input type='email' defaultValue={email} onBlur={handleChange} name='email' />
                 </div>
                 <div>
                     <label htmlFor='message'>Message:</label>
-                    <textarea name='message' defaultValue={message} onChange={handleChange} rows='5' />
+                    <textarea name='message' defaultValue={message} onBlur={handleChange} rows='5' />
                 </div>
+                {errorMessage && (
+                    <div>
+                        <p className='error-text'>{errorMessage}</p>
+                    </div>
+                )}
                 <button type='submit'>Submit</button>
             </form>
         </section>
