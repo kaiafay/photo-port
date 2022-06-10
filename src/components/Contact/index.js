66e8a6f07eec7542e53d560fd4cb6ecd2404142e
Kaia Scheirman <kfscheirman18@gmail.com>
2022-06-09 18:03:59 -0700

    initialize and sync form data with ContactForm component's state

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ContactForm() {
+    // set initial state to empty strings for input fields
+    const [formState, setFormState] = useState({ name: '', email: '', message: ''});
+    // destructure formState object into its named properties
+    const { name, email, message } = formState;
+
+    // sync formState component with user input from the DOM
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+    };
+
     return (
         <section>
             <h1>Contact Me</h1>
             <form id='contact-form'>
                 <div>
                     <label htmlFor='name'>Name:</label>
-                    <input type='text' name='name' />
+                    <input type='text' defaultValue={name} onChange={handleChange} name='name' />
                 </div>
                 <div>
                     <label htmlFor='email'>Email address:</label>
-                    <input type='email' name='email' />
+                    <input type='email' defaultValue={email} onChange={handleChange} name='email' />
                 </div>
                 <div>
                     <label htmlFor='message'>Message:</label>
-                    <textarea name='message' rows='5' />
+                    <textarea name='message' defaultValue={message} onChange={handleChange} rows='5' />
                 </div>
                 <button type='submit'>Submit</button>
             </form>
